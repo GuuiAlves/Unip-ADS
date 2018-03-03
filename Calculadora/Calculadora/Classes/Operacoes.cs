df4dc4eee183c158c0de0eb066e3e0318e57cc84
Guilherme Alves <36598083+GuuiAlves@users.noreply.github.com>
2018-03-03 13:06:23 -0300

    Calculadora com Classe Controle e Operações
    Calculadora organizada com classes e tratativas de erros.

---
+++ Calculadora/Calculadora/Classes/Operacoes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Classes
+{
+    class Operacoes
+    {
+        public Double Valor1;
+        public Double Valor2;
+        public Double resposta;
+        public String resultado;
+
+        public String Somar()
+        {
+            resposta = Valor1 + Valor2;
+            resultado = resposta.ToString();
+            return resultado;
+        }
+        public String Subtrair()
+        {
+            resposta = Valor1 - Valor2;
+            resultado = resposta.ToString();
+            return resultado;
+        }
+        public String Dividir()
+        {
+            if(this.Valor1 == 0)
+            {
+                return resultado = "Erro, divisão por zero!";
+            }
+            resposta = Valor1 / Valor2;
+            resultado = resposta.ToString();
+            return resultado;
+        }
+        public String Multiplicar()
+        {
+            resposta = Valor1 * Valor2;
+            resultado = resposta.ToString();
+            return resultado;
+        }
+    }
+}
