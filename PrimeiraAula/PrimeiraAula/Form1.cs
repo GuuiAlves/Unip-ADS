cf0c622f0f536557af961803843f1c578d08b550
Guilherme Alves <36598083+GuuiAlves@users.noreply.github.com>
2018-03-09 21:26:17 -0300

    Add files via upload

---
+++ PrimeiraAula/PrimeiraAula/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PrimeiraAula.Modelo;
+
+namespace PrimeiraAula
+{
+    public partial class frmPrincipal : Form
+    {
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            Controle controle = new Controle(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "+");
+            lblResultado.Text = controle.Resposta;
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+       
+    }
+}
---
+++ PrimeiraAula/PrimeiraAula/Modelo/Controle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeiraAula.Modelo
+{
+    public class Controle
+    {
+        #region Atributos
+        private String num1;
+        private String num2;
+        private String op;
+        private String resposta;
+        #endregion
+
+        #region Construtores
+        public Controle(String num1, String num2, String op)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.op = op;
+            Calcular();
+        }
+        #endregion
+
+        #region Metodos
+        public void Calcular()
+        {
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0;
+            try
+            {
+                n1 = Convert.ToDouble(this.num1);
+                n2 = Convert.ToDouble(this.num2);
+                if (this.op.Equals("+"))
+                    resultado = n1 + n2;
+                if (this.op.Equals("-"))
+                    resultado = n1 - n2;
+                if (this.op.Equals("*"))
+                    resultado = n1 * n2;
+                resposta = resultado.ToString("0.##");
+                if (this.op.Equals("/"))
+                    if (n2 == 0)
+                        this.resposta = "Divisão por zero";
+                    else
+                    {
+                        resultado = n1 / n2;
+                        this.resposta = resultado.ToString("0.##");
+                    }
+            }
+            catch (FormatException)
+            {
+                resposta = "Digite um número válido";
+            }
+        }
+        #endregion
+
+        #region Propriedades
+        public String Resposta
+        {
+            get { return resposta; }
+        }
+        #endregion
+    }
+}
